30259d9854a2d25fb1885a1e08f36dee3c074da7
Arthur Machado <machadoparthur1@gmail.com>
2020-09-24 11:34:18 -0300

    Change counter

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=15;i++){
+      for(int i=1;i<=3;i++){
          System.out.println("Testando o build automatico do Jenkins "+i);
       }
    }
